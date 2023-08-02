1349df82ff8eba74789983816a9beec55eca9eeb
seon <sunkyoung.dev@gmail.com>
2023-08-02 17:41:15 +0900

    1.변수와 상수

---
+++ Js/1.core-js/1.basic/1.변수와상수.js
@@ -0,0 +1,34 @@
+//변수 선언
+let userName='뽀로로';
+//재할당
+userName='루피';
+
+//변수 콘솔창 출력
+console.log(userName);
+
+//상수: 불변의 데이터
+const saleRate=0.2;
+
+//var : 예전에 변수 선언시 썼던것 -> 권장하지 않음
+var a=10;
+
+//이름 관례(convention)
+//개발자끼리의 약속
+
+//변수, 함수 이름: camel case
+//첫글자소문자, 그다음 단어 시작을 대문자로
+myPhoneCase='0000';
+//스네이크 케이스
+my_phone_number=1111;
+
+//들여쓰기 공백은 2칸
+if(true){
+  if(true){
+    console.log('hello');
+  }
+
+}
+
+//키워드(예약어 쓰지 말것)
+
+//식별자 이름은 구체적이고 명확하고 일관성 있게 지을 것!
